1204ad6eb9204e414b98d0c671adbae9b96fb45b
MelanieZeng <37940713+MelanieZeng@users.noreply.github.com>
2018-09-19 17:05:56 -0400

    Conversation Update

--- Gemfile
+++ Gemfile
@@ -7,4 +7,7 @@ gem 'dotenv-rails', :groups => [:development, :test]
 
 gem 'twilio-ruby', ">5.0.0"
 
+gem 'rubygems'
+gem 'rest_client'
+
 ruby '2.4.4'
--- app.rb
+++ app.rb
@@ -17,6 +17,27 @@ greetings_an = ["Good afternoon"]
 greetings_en = ["Good evening"]
 secret_code = "melanieiscool"
 
+get '/' do 
+	session["visits"] ||= 0
+	session["visits"] = session["visits"] + 1 
+	time = Time.now
+	if session[:first_name].nil?
+		'What is the best food and drink for a summer night when you are sitting at the bench in your backyard and enjoying the wind gently blowing through your hair? Show me a selfie and I will customize the best food + drink combincation for you! Sign up to chat with me now! <br />Total visits on our website: ' + session["visits"].to_s
+    else 
+    	if session["visits"] > 10 #make a user a VIP after they visit the website more than 10 times
+    		session[:first_name] + ', You are a VIP now!' + '<br /> You have visited ' + session["visits"].to_s + ' times as of ' + time.strftime("%A %B %d, %Y %H:%M")
+    	else #show different greetings based on the time during a day
+			if time.hour >= 5 and time.hour <= 14
+				greetings_mn.sample + ', ' + session[:first_name] + '. <br /> You have visited ' + session["visits"].to_s + ' times as of ' + time.strftime("%A %B %d, %Y %H:%M")
+			elsif time.hour > 14 and time.hour <= 18
+				greetings_an.sample + ', ' + session[:first_name] + '. <br /> You have visited ' + session["visits"].to_s + ' times as of ' + time.strftime("%A %B %d, %Y %H:%M")
+			else
+				greetings_en.sample + ', ' + session[:first_name] + '. <br /> You have visited ' + session["visits"].to_s + ' times as of ' + time.strftime("%A %B %d, %Y %H:%M")
+			end
+		end
+	end
+end
+
 #modify signup page
 get '/signup' do
 	if params[:code] == secret_code
@@ -45,38 +66,26 @@ post '/signup' do
 		end
 	end
 end
- 
-#modify about page 
-get '/about' do
-	session["visits"] ||= 0
-	session["visits"] = session["visits"] + 1 
-	time = Time.now
-	if session[:first_name].nil?
-		'Is picking food an unsolved problem in your day? "Eatappy" is the food guide for you! Simply play a fun game by answering a few questions and I will tell you what kind of food you should get today. Eating is happy. Enjoy it! <br />Total visits on our website: ' + session["visits"].to_s
-    else 
-    	if session["visits"] > 10 #make a user a VIP after they visit the website more than 10 times
-    		session[:first_name] + ', You are a VIP now!' + '<br /> You have visited ' + session["visits"].to_s + ' times as of ' + time.strftime("%A %B %d, %Y %H:%M")
-    	else #show different greetings based on the time during a day
-			if time.hour >= 5 and time.hour <= 14
-				greetings_mn.sample + ', ' + session[:first_name] + '. <br /> You have visited ' + session["visits"].to_s + ' times as of ' + time.strftime("%A %B %d, %Y %H:%M")
-			elsif time.hour > 14 and time.hour <= 18
-				greetings_an.sample + ', ' + session[:first_name] + '. <br /> You have visited ' + session["visits"].to_s + ' times as of ' + time.strftime("%A %B %d, %Y %H:%M")
-			else
-				greetings_en.sample + ', ' + session[:first_name] + '. <br /> You have visited ' + session["visits"].to_s + ' times as of ' + time.strftime("%A %B %d, %Y %H:%M")
-			end
-		end
-	end
-end
 
 #modify incoming/sms page
 get '/incoming/sms' do
 	session["counter"] ||= 1
+	session[:First_name] = params[:First_name]
+	time = Time.now
 
 	body = params[:Body] || ""
 
 	if session["counter"] == 1
-		message = "Hey, it's great to hear your first message! I am Eatappy. If picking food is an unsolved problem for your daily life, I am here to help you! 😋 Would you like to pick your food today? Reply yes or yeah to get started. "
-		media = "https://media0.giphy.com/media/3o7TKMt1VVNkHV2PaE/giphy.gif" 
+		if time.hour >= 5 and time.hour <= 10
+    		message = greetings_mn.sample + session[:First_name] + ", it's great to hear your first message! I am Eatappy 😋. Would you like to pick your breakfast? Send me a seflie that best describes your mood now! "
+			media = "https://media0.giphy.com/media/3o7TKMt1VVNkHV2PaE/giphy.gif"
+		elsif time.hour > 10 and time.hour <= 16
+			message = greetings_an.sample + session[:First_name] + ", it's great to hear your first message! I am Eatappy 😋. Would you like to pick your lunch or hightea? Send me a seflie that best describes your mood now! "
+			media = "https://media0.giphy.com/media/3o7TKMt1VVNkHV2PaE/giphy.gif"
+		else
+			message = greetings_en.sample + session[:First_name] + ", it's great to hear your first message! I am Eatappy 😋. Would you like to pick your dinner? Send me a seflie that best describes your mood now! "
+			media = "https://media0.giphy.com/media/3o7TKMt1VVNkHV2PaE/giphy.gif"
+		end
     else
     	message = determine_response body
     end
@@ -103,30 +112,12 @@ get '/incoming/sms' do
     twiml.to_s
 end
 
-#modify test/conversation page
-get '/test/conversation' do
-	body = params[:body]
-	from = params[:from]
-	if body.nil? or from.nil?
-		puts 'Input for body and from are missing. Please give an input for body and from in the URL such as follows <br />localhost:4567/test/conversation?body=Hi&From=1234567789'
-		return
-	else
-		determine_response body
-	end
-end
-
 #error 404
 error 404 do
 	"Access Forbidden"
 end
 
-#redirect
-get '/' do 
-	redirect '/about'
-end
-
 #methods
-
 def determine_response body
 	body = body.downcase.strip
 	hi_vocabs = ["hi", "hello", "hey"]
@@ -135,36 +126,34 @@ def determine_response body
 	where_vocabs = ["where", "location", "city"]
 	when_vocabs = ["when", "created", "born", "made"]
 	why_vocabs = ["why", "purpose", "for", "meaning"]
-	yes_vocabs = ["yes", "yeah", "yup", "sure", "sounds good", "ok"]
+	yes_vocabs = ["yes", "yeah", "yup", "sure", "sounds good", "ok", "I'd love to"]
 
-	if body == 'hi' or has_vocab_in_sentence body, hi_vocabs
-		'Hi, I am "Eatappy". I am the food guide for you!'
-	elsif body == 'who' or has_vocab_in_sentence body, who_vocabs
-		'I am a MeBot of Melanie Zeng. Want to know more about me? Try putting "fact" as an input for body!'
-	elsif body == 'what' or has_vocab_in_sentence body, what_vocabs
-		'Simply play a fun game with me by answering a few questions and I will help you to pick your food today!'
-	elsif body == 'where' or has_vocab_in_sentence body, where_vocabs
-		'I live in Pittsburgh!'
-	elsif body == 'when' or has_vocab_in_sentence body, when_vocabs
-		'I was created in Fall 2018!'
-	elsif body == 'why' or has_vocab_in_sentence body, why_vocabs
-		'I was made by Melanie Zeng for her Programming for Online Prototypes Class!'
+	if has_vocab_in_sentence body, hi_vocabs
+		'Hi, I am "Eatappy". I am the food guide for you! Send me a selfie and I will customize your food and drink based on your mood. '
+	elsif has_vocab_in_sentence body, who_vocabs
+		'I am a MeBot of Melanie Zeng. Reply "fact" to know more about Melanie! '
+	elsif has_vocab_in_sentence body, what_vocabs
+		'Let me know how you feel by showing me a selfie and I will help you to pick your food and drink! '
+	elsif has_vocab_in_sentence body, where_vocabs
+		'I live in Pittsburgh! '
+	elsif has_vocab_in_sentence body, when_vocabs
+		'I was created in Fall 2018! '
+	elsif has_vocab_in_sentence body, why_vocabs
+		'I was made by Melanie Zeng for her Programming for Online Prototypes Class! '
 	elsif body == 'joke'
 		file = File.open("jokes.txt", "r")
 		array_of_lines = IO.readlines("jokes.txt")
-		return array_of_lines.sample + "\n Tell me if you like the joke by putting 'jokewasgood' as an input for body. If not, put 'jokewasbad' and I will try harder!"
+		return array_of_lines.sample + "\n Reply 'jokewasgood' if you like it or 'jokewasbad' if you don't. Don't be mean to me please!"
 	elsif body == 'jokewasgood'
 		'Thank you for the complement. I am glad that you like it!'
 	elsif body == 'jokewasbad'
 		'Sorry, I am not good at jokes. But I will try harder :)'
-	elsif body == 'fact' or body == 'Fact'
+	elsif body == 'fact'
 		file = File.open("facts.txt", "r")
 		array_of_facts = IO.readlines("facts.txt")
 		return array_of_facts.sample
-	elsif has_vocab_in_sentence body, yes_vocabs
-		'Send me a selfie and let me see how you look now!'
 	else
-		"Oops! I didn't get that. Please put one of the follows as input for body: hi, who, what, where, when, why, joke, help and fact. "
+		"Oops! I didn't get that. Reply 'hi', 'who', 'what' or 'why' if you want to learn more about me. "
 	end
 end
 
